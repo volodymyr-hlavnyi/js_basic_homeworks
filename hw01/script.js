0c530ef77549a9047d35fb62a45aa39a471fa1f0
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2023-12-01 18:31:09 +0100

    HW 1

---
+++ hw01/script.js
@@ -0,0 +1,38 @@
+// 'number' + 23 + 32
+console.log('number' + 23 + 32)
+// 'number' string
+// plus 23 plus 23
+// result -> string number2323
+
+// 41 + 1 + 'number'
+// null + 1
+// 'five' + + 'two'
+// 2 && 7
+// +'40' + +'2';
+// '10' - 5 === 6;
+// true + false
+// '4px' - 3
+// '4' - 3
+// '2' + 3 ** 2;
+// 12 / '6'
+// 23 + 42 + 'number'
+// '10' + (5 === 6);
+// 'number' + 15 + 3
+//
+//
+// undefined + 1;
+// 'true' == true
+// false == 'false'
+// null == ''
+// 3 ** (9 / 3);
+// !!'false' == !!'true'
+// 0 || '0' && 1
+// 1 < 2 < 3
+// 'foo'+ + +'bar'
+// 3 ** 2 / 3;
+// 1 < 2 > 3
+// (+null == false) < 1;
+// false && true || true
+// false && (true || true);
+// (+null == false) < 1 ** 5;
+
